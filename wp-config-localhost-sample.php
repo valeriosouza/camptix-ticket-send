e81fd5867af6bb4030f75b83682f10c194625c54
Leo Baiano <ljunior2005@gmail.com>
2014-11-04 18:42:35 -0300

    issue-#12

--- wp-config-localhost-sample.php
+++ wp-config-localhost-sample.php
@@ -18,10 +18,10 @@
 
 	// ** MySQL settings - You can get this info from your web host ** //
 	/** The name of the database for WordPress */
-	define('DB_NAME', 'wpcheckin');
+	define('DB_NAME', '');
 
 	/** MySQL database username */
-	define('DB_USER', 'root');
+	define('DB_USER', '');
 
 	/** MySQL database password */
 	define('DB_PASSWORD', '');
